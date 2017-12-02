40b15ef601ea7d5f90e87f620dbc7689c5478c85
Blaire Ness <blaire.ness@stonybrook.edu>
2017-12-01 20:21:30 -0500

    Finished visibilities.py and made changes to lab3_visibilities.txt

--- Lab3/visibilities.py
+++ Lab3/visibilities.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
@@ -7,17 +8,30 @@ filename = 'lab3_visibilities.txt'
 baseline, vis, baserr, viserr = np.loadtxt(filename, unpack=True)
 
 # Define sinc function
-def vis_sinc(b,a):
-    return abs(np.sin(np.pi*b*a)/(np.pi*b))
+def vis_sinc(b):
+    a = 0.009
+    return (1/a)*abs(np.sin(np.pi*b*a)/(np.pi*b))
 
-popt, pcov = curve_fit(vis_sinc,baseline,vis)
-print(popt)
-print(pcov)
+extra_baseline = []
+vis_expected = []
 
-fitted_data = vis_sinc(baseline,*popt)
+n = 0
 
-plt.plot(baseline, vis, linestyle='none',marker='o')
+while n <= 300:
+    extra_baseline.append(n)
+    vis_expected.append(vis_sinc(n))
+    n = n+1
+    
+#for i in range(0, len(baseline)):
+#    print(baseline[i], vis[i])
+#    print('alpha = ', alpha(baseline[i],vis[i]))
+
+plt.errorbar(baseline, vis, xerr=baserr, yerr=viserr, linestyle='none',marker='o', label="Measured Values")
+plt.ylim(0.0,0.4)
+plt.xlim(50.0,270.0)
 plt.xlabel('Baseline (cm)')
 plt.ylabel('Visibility Amplitude')
-plt.plot(baseline,fitted_data, linestyle="--")
+plt.plot(extra_baseline, vis_expected, linestyle="-", label="Expected sinc function")
+plt.legend()
 plt.show()
+plt.savefig('Visibilities.png')
