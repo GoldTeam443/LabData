67ba4d49601a0fd5cc3a7a3a29cca8d9ffbfa25d
Blaire Ness <blaire.ness@stonybrook.edu>
2017-12-01 20:24:15 -0500

    Made final changes to visibilities.py

--- Lab3/visibilities.py
+++ Lab3/visibilities.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
@@ -17,15 +16,13 @@ vis_expected = []
 
 n = 0
 
+# Calculate expected values
 while n <= 300:
     extra_baseline.append(n)
     vis_expected.append(vis_sinc(n))
     n = n+1
     
-#for i in range(0, len(baseline)):
-#    print(baseline[i], vis[i])
-#    print('alpha = ', alpha(baseline[i],vis[i]))
-
+# Plot values
 plt.errorbar(baseline, vis, xerr=baserr, yerr=viserr, linestyle='none',marker='o', label="Measured Values")
 plt.ylim(0.0,0.4)
 plt.xlim(50.0,270.0)
@@ -33,5 +30,6 @@ plt.xlabel('Baseline (cm)')
 plt.ylabel('Visibility Amplitude')
 plt.plot(extra_baseline, vis_expected, linestyle="-", label="Expected sinc function")
 plt.legend()
-plt.show()
+#plt.show()
 plt.savefig('Visibilities.png')
+print("Saved to Visibilities.png")
