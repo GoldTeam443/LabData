4ac256ae1701d8c9f7a44a1e282aa792c595eff1
Blaire Ness <blaire.ness@stonybrook.edu>
2017-12-02 19:23:35 -0500

    Corrected sinc function in visibilities.py

--- Lab3/visibilities.py
+++ Lab3/visibilities.py
@@ -7,29 +7,33 @@ filename = 'lab3_visibilities.txt'
 baseline, vis, baserr, viserr = np.loadtxt(filename, unpack=True)
 
 # Define sinc function
-def vis_sinc(b):
-    a = 0.009
-    return (1/a)*abs(np.sin(np.pi*b*a)/(np.pi*b))
+def vis_sinc(b,a):
+    #a = 0.009 (expected value)
+    return abs(np.sin(np.pi*b*a)/(np.pi*b*a))
 
 extra_baseline = []
-vis_expected = []
+vis_fit = []
 
 n = 0
+
+popt, pcov = curve_fit(vis_sinc, baseline, vis, sigma=viserr, p0=0.009)
+perr = np.sqrt(np.diag(pcov))
+print("alpha=",popt, "alpha_error=",perr)
 
 # Calculate expected values
 while n <= 300:
     extra_baseline.append(n)
-    vis_expected.append(vis_sinc(n))
+    vis_fit.append(vis_sinc(n,*popt))
     n = n+1
-    
+
 # Plot values
 plt.errorbar(baseline, vis, xerr=baserr, yerr=viserr, linestyle='none',marker='o', label="Measured Values")
 plt.ylim(0.0,0.4)
-plt.xlim(50.0,270.0)
-plt.xlabel('Baseline (cm)')
+plt.xlim(20.0,120.0)
+plt.xlabel('B_lambda') #Unitless, is the baseline(cm)/lambda(cm)
 plt.ylabel('Visibility Amplitude')
-plt.plot(extra_baseline, vis_expected, linestyle="-", label="Expected sinc function")
+plt.plot(extra_baseline, vis_fit, linestyle="-", label="Fit visibility function")
 plt.legend()
 #plt.show()
-plt.savefig('Visibilities.png')
-print("Saved to Visibilities.png")
+plt.savefig('vis_fit.png')
+print("Saved to vis_fit.png")
